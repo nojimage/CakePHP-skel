c81d52dfc9c4fc2bbd2216a25723fc5a14c25d51
nojimage <nojimage@gmail.com>
2011-04-19 14:16:14 +0900

    add a comment for don't replace CAKE_CORE_INCLUDE_PATH

--- basic/webroot/test.php
+++ basic/webroot/test.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Web Access Frontend for TestSuite
  *
@@ -22,36 +23,35 @@ ini_set('display_errors', 1);
 /**
  * Use the DS to separate the directories in other defines
  */
-	if (!defined('DS')) {
-		define('DS', DIRECTORY_SEPARATOR);
-	}
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
 /**
  * These defines should only be edited if you have cake installed in
  * a directory layout other than the way it is distributed.
  * When using custom settings be sure to use the DS and do not add a trailing DS.
  */
-
 /**
  * The full path to the directory which holds "app", WITHOUT a trailing DS.
  *
  */
-	if (!defined('ROOT')) {
-		define('ROOT', dirname(dirname(dirname(__FILE__))));
-	}
+if (!defined('ROOT')) {
+    define('ROOT', dirname(dirname(dirname(__FILE__))));
+}
 /**
  * The actual directory name for the "app".
  *
  */
-	if (!defined('APP_DIR')) {
-		define('APP_DIR', basename(dirname(dirname(__FILE__))));
-	}
+if (!defined('APP_DIR')) {
+    define('APP_DIR', basename(dirname(dirname(__FILE__))));
+}
 /**
  * The absolute path to the "cake" directory, WITHOUT a trailing DS.
  *
  */
-	if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-		define('CAKE_CORE_INCLUDE_PATH', ROOT);
-	}
+if (!defined('CAKE_CORE_INCLUDE_PATH')) {
+    define('CAKE_CORE_INCLUDE_PATH', ROOT /* DON'T REPLACE ME */);
+}
 
 /**
  * Editing below this line should not be necessary.
@@ -59,33 +59,33 @@ ini_set('display_errors', 1);
  *
  */
 if (!defined('WEBROOT_DIR')) {
-	define('WEBROOT_DIR', basename(dirname(__FILE__)));
+    define('WEBROOT_DIR', basename(dirname(__FILE__)));
 }
 if (!defined('WWW_ROOT')) {
-	define('WWW_ROOT', dirname(__FILE__) . DS);
+    define('WWW_ROOT', dirname(__FILE__) . DS);
 }
 if (!defined('CORE_PATH')) {
-	if (function_exists('ini_set') && ini_set('include_path', CAKE_CORE_INCLUDE_PATH . PATH_SEPARATOR . ROOT . DS . APP_DIR . DS . PATH_SEPARATOR . ini_get('include_path'))) {
-		define('APP_PATH', null);
-		define('CORE_PATH', null);
-	} else {
-		define('APP_PATH', ROOT . DS . APP_DIR . DS);
-		define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
-	}
+    if (function_exists('ini_set') && ini_set('include_path', CAKE_CORE_INCLUDE_PATH . PATH_SEPARATOR . ROOT . DS . APP_DIR . DS . PATH_SEPARATOR . ini_get('include_path'))) {
+        define('APP_PATH', null);
+        define('CORE_PATH', null);
+    } else {
+        define('APP_PATH', ROOT . DS . APP_DIR . DS);
+        define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
+    }
 }
 if (!include(CORE_PATH . 'cake' . DS . 'bootstrap.php')) {
-	trigger_error("CakePHP core could not be found.  Check the value of CAKE_CORE_INCLUDE_PATH in APP/webroot/index.php.  It should point to the directory containing your " . DS . "cake core directory and your " . DS . "vendors root directory.", E_USER_ERROR);
+    trigger_error("CakePHP core could not be found.  Check the value of CAKE_CORE_INCLUDE_PATH in APP/webroot/index.php.  It should point to the directory containing your " . DS . "cake core directory and your " . DS . "vendors root directory.", E_USER_ERROR);
 }
 
 $corePath = App::core('cake');
 if (isset($corePath[0])) {
-	define('TEST_CAKE_CORE_INCLUDE_PATH', rtrim($corePath[0], DS) . DS);
+    define('TEST_CAKE_CORE_INCLUDE_PATH', rtrim($corePath[0], DS) . DS);
 } else {
-	define('TEST_CAKE_CORE_INCLUDE_PATH', CAKE_CORE_INCLUDE_PATH);
+    define('TEST_CAKE_CORE_INCLUDE_PATH', CAKE_CORE_INCLUDE_PATH);
 }
 
 if (Configure::read('debug') < 1) {
-	die(__('Debug setting does not allow access to this url.', true));
+    die(__('Debug setting does not allow access to this url.', true));
 }
 
 require_once CAKE_TESTS_LIB . 'cake_test_suite_dispatcher.php';
